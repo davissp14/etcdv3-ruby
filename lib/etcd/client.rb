cf7e7cd4b4e13b95f7b36b9d4ebef919dc2c74ec
Shaun Davis <davissp14@gmail.com>
2017-03-28 08:55:12 -0500

    Hacking this to make it easier to test SNI

--- lib/etcd/client.rb
+++ lib/etcd/client.rb
@@ -1,14 +1,24 @@
 module Etcd
   class Client
-    def initialize(hostname="127.0.0.1:2379", certs=:this_channel_is_insecure)
+
+    def initialize(hostname="127.0.0.1:2379", certs=:this_channel_is_insecure, servername_override=nil)
       @hostname = hostname
-      @certs = certs
+      case certs
+      when :this_channel_is_insecure
+        @certs = :this_channel_is_insecure
+      when :stub
+        @certs = GRPC::Core::ChannelCredentials.new
+      else
+        raise("Cert type not available yet.")
+      end
+      @channel_args = {}
+      @channel_args[GRPC::Core::Channel::SSL_TARGET] = servername_override if servername_override
       @metadata = {}
-      @conn ||= Etcdserverpb::KV::Stub.new("#{hostname}", certs)
+      @conn ||= Etcdserverpb::KV::Stub.new("#{hostname}", @certs, channel_args: @channel_args)
     end
 
     def authenticate(user, password)
-      auth = Etcdserverpb::Auth::Stub.new(@hostname, @certs)
+      auth = Etcdserverpb::Auth::Stub.new(@hostname, @certs, channel_args: @channel_args)
       result = auth.authenticate(Authpb::User.new(name: user, password: password))
       @metadata = {token: result.token}
     rescue GRPC::InvalidArgument => exception
