0a2fdfb2fd676ab531e47001a1af354f10ae86fb
Shaun Davis <davissp14@gmail.com>
2017-03-28 21:54:32 -0500

    Bulk commit

--- lib/etcd.rb
+++ lib/etcd.rb
@@ -2,10 +2,6 @@
 require 'grpc'
 require 'uri'
 
-require 'etcd/etcdrpc/rpc_services_pb'
-require 'etcd/client'
-require 'etcd/auth'
-
 # For Debugging Export the following
 # `export GRPC_VERBOSITY=DEBUG`
 # `export GRPC_TRACE=all`
---
+++ lib/etcd/kv.rb
@@ -0,0 +1,23 @@
+
+module Etcd
+
+  class KV
+
+    def initialize(hostname, port, certs)
+      @stub = Etcdserverpb::KV::Stub.new("#{hostname}:#{port}", certs)
+    end
+
+    def put(key, value, metadata)
+      kv = Etcdserverpb::PutRequest.new(key: key, value: value)
+      @stub.put(kv, metadata: metadata)
+    end
+
+    def range(key, range_end, metadata)
+      kv = Etcdserverpb::RangeRequest.new(key: key, range_end: range_end)
+      result = @stub.range(kv, metadata: @metadata)
+      result.kvs
+    end
+
+  end
+
+end
