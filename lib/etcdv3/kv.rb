ebae36fcb08d84c0201941664b46d0fe71e9bd09
Shaun Davis <davissp14@gmail.com>
2017-04-03 09:37:30 -0500

    Experimental refactor.  Possibly too much magic

--- lib/etcdv3/kv.rb
+++ lib/etcdv3/kv.rb
@@ -1,25 +1,25 @@
 
 class Etcd
-  class KV
-    def initialize(hostname, port, credentials, metadata={})
-      @stub = Etcdserverpb::KV::Stub.new("#{hostname}:#{port}", credentials)
-      @metadata = metadata
-    end
+  module KV
+    STUB = Etcdserverpb::KV::Stub
 
     def put(key, value)
-      kv = Etcdserverpb::PutRequest.new(key: key, value: value)
-      @stub.put(kv, metadata: @metadata)
-    rescue GRPC::PermissionDenied, GRPC::InvalidArgument => exception
-      puts exception.message
-      false
+      resolve_request('PutRequest', 'put',
+        attributes: {
+          key: key,
+          value: value
+        }
+      )
     end
 
     def get(key, range_end="")
-      kv = Etcdserverpb::RangeRequest.new(key: key, range_end: range_end)
-      @stub.range(kv, metadata: @metadata).kvs
-    rescue GRPC::PermissionDenied, GRPC::InvalidArgument => exception
-      puts exception.message
-      false
+      resolve_request('RangeRequest', 'range',
+        attributes: {
+          key: key,
+          range_end: range_end
+        }
+      ).kvs
     end
+
   end
 end
---
+++ lib/etcdv3/request_handler.rb
@@ -0,0 +1,21 @@
+
+class Etcd
+  class RequestHandler
+
+    def initialize(target, uri, creds, metadata)
+      @stub = target::STUB.new(uri, creds)
+      @metadata = metadata
+      self.class.include(target)
+    end
+
+    def resolve_request(req, target, attributes: {}, auth: true )
+      const = Object.const_get("Etcdserverpb::#{req}")
+      request = const.new(attributes)
+      if auth
+        @stub.send(target, request, metadata: @metadata)
+      else
+        @stub.send(target, request)
+      end
+    end
+  end
+end
