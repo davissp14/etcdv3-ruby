4b53ffe7e9b8f93579cc35dd967c8d86ce15a368
Shaun Davis <davissp14@gmail.com>
2017-04-03 17:31:51 -0500

    Ditching extra spacing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ require 'etcdv3'
 require 'helpers/test_instance'
 
 RSpec.configure do |config|
-  
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
