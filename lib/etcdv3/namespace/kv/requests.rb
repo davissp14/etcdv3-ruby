1c65bce3b514323c544b0afb76ed435ffabebbd7
manabu-s <manabu-s@iij.ad.jp>
2023-12-07 18:32:10 +0900

    range_end is string

--- lib/etcdv3/namespace/kv/requests.rb
+++ lib/etcdv3/namespace/kv/requests.rb
@@ -32,14 +32,14 @@ class Etcdv3::Namespace::KV
       Etcdserverpb::RangeRequest.new(opts)
     end
 
-    def del_request(key, range_end=nil)
+    def del_request(key, range_end='')
       key = prepend_prefix(@namespace, key)
       # In order to enforce the scope of the specified namespace, we are going to
       # intercept the zero-byte reference and re-target everything under the given namespace.
       if range_end =~ /\x00/
         range_end = (@namespace[0..-2] + (@namespace[-1].ord + 1).chr)
       else 
-        range_end = prepend_prefix(@namespace, range_end) if range_end
+        range_end = prepend_prefix(@namespace, range_end) unless range_end.empty?
       end
       Etcdserverpb::DeleteRangeRequest.new(key: key, range_end: range_end)
     end
